51b51036e13130d69624bb8528d3fc470c155340
Andreas Pabst <kontakt@andreas-pabst.de>
2024-08-01 16:48:40 +0200

    Added a few more components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
-import RexAlert from '@/components/Rex/Alert/Alert.vue';
-import RexButton from '@/components/Rex/Button/Button.vue';
-import RexInputText from '@/components/Rex/Input/InputText.vue';
-import RexIcon from '@/components/Rex/Icon/Icon.vue';
-import RexIconClose from '@/components/Rex/Icon/IconClose.vue';
-import RexApp from '@/components/Rex/App/App.vue';
+import {createRexUI} from "@/plugins/Vue3RexUI";
 
 /**
  * Create the UI Library
  */
-export function createRexUI() {
-
-    return {
-        install(app) {
-            // register RexAlert, RexButton, RexInputText, RexApp
-            app.component('RexAlert', RexAlert);
-            app.component('RexButton', RexButton);
-            app.component('RexIcon', RexIcon);
-            app.component('RexIconClose', RexIconClose);
-            app.component('RexInputText', RexInputText);
-            app.component('RexApp', RexApp);
-        },
-    };
-}
+export default createRexUI
--- src/plugins/Vue3RexUI.ts
+++ src/plugins/Vue3RexUI.ts
@@ -1,10 +1,15 @@
 import type { App } from 'vue';
 import RexAlert from '@/components/Rex/Alert/Alert.vue';
+import RexApp from '@/components/Rex/App/App.vue';
 import RexButton from '@/components/Rex/Button/Button.vue';
+import RexCol from '@/components/Rex/Grid/Col.vue';
+import RexContainer from '@/components/Rex/Grid/Container.vue';
+import RexFooter from '@/components/Rex/Footer/Footer.vue';
+import RexHeader from '@/components/Rex/Header/Header.vue';
 import RexInputText from '@/components/Rex/Input/InputText.vue';
 import RexIcon from '@/components/Rex/Icon/Icon.vue';
 import RexIconClose from '@/components/Rex/Icon/IconClose.vue';
-import RexApp from '@/components/Rex/App/App.vue';
+import RexRow from '@/components/Rex/Grid/Row.vue';
 
 /**
  * Create the UI Library
@@ -13,13 +18,17 @@ export function createRexUI() {
 
     return {
         install(app: App) {
-            // register RexAlert, RexButton, RexInputText, RexApp
+            app.component('RexApp', RexApp);
             app.component('RexAlert', RexAlert);
             app.component('RexButton', RexButton);
+            app.component('RexCol', RexCol);
+            app.component('RexContainer', RexContainer);
+            app.component('RexFooter', RexFooter);
+            app.component('RexHeader', RexHeader);
             app.component('RexIcon', RexIcon);
             app.component('RexIconClose', RexIconClose);
             app.component('RexInputText', RexInputText);
-            app.component('RexApp', RexApp);
+            app.component('RexRow', RexRow);
         },
     };
 }
